168d9a3225fe5293f6937ac15a09b541d7e02954
cabassa <rivera.irving@gmail.com>
2016-04-12 21:50:55 -0400

    test

---
+++ P11.py
@@ -0,0 +1,13 @@
+numbers = range(1,21)
+print numbers
+sum = 0
+for counter in numbers:
+    sum = sum + counter
+print
+print "Sumatoria:", sum
+
+print
+my_names = ['Joe', 'Bob','Ned','Jil']
+print my_names[0]
+print my_names[2]
+print my_names[3]
